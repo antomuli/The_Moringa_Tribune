042fb05646a63c1f547f842ef0780a739a23b07e
Anthony Muli <mulianthony561@gmail.com>
2020-03-03 15:39:42 +0300

    Imported the EmailAlternatives class from the django mail module that is responsible for sending emails

---
+++ news/emails.py
@@ -0,0 +1,15 @@
+from django.core.mail import EmailMultiAlternatives
+fro django.template.loader import render_to_string
+
+def send_welcome_email(name,receiver):
+    #Creating message subject and sender
+    subject = 'Welcome to the MoringaTribune NewsLetter'
+    sender = 'james@moringaschool.com'
+
+    #Passing in the context variables
+    text_content = render_to_string('email/newsemail.txt',{"name":name})
+    html_content = render_to_string('email/newsemail.html',{"name":name})
+    msg = EmailMultiAlternatives(subject,text_content,sender,[receiver])
+    msg.attach_alternative(html_content,'text/html')
+    msg.send()
+    
