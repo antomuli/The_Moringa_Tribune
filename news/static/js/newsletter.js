490564f9c760a90c5173ffda013fdc13d48f9cf9
Anthony Muli <mulianthony561@gmail.com>
2020-03-11 11:21:56 +0300

    Created the AJAX request

--- news/static/js/newsletter.js
+++ news/static/js/newsletter.js
@@ -1,6 +1,20 @@
 $(document).ready(function(){
     $('form').submit(function(event){
       event.preventDefault()
+      form = $("form")
+
+      $.ajax({
+        'url':'/ajax/newsletter/',
+        'type':'POST',
+        'data':form.serialize(),
+        'datatype':'json',
+        'success':function(data){
+            alert(data['success'])
+        },
+
+      })//END of Ajax method
+      $('#id_your_name').val('')
+      $("#id_email").val('')
     }) // End of submit event
   
   }) // End of document ready function
