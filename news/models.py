4c7ec1d7f06d38ac56502276ea6b5d44b906af62
Anthony Muli <mulianthony561@gmail.com>
2020-03-11 13:34:13 +0300

    Created a new model MoringaMerch that will be used to create products that Moringa School will be creating and made migrations to the database

--- news/models.py
+++ news/models.py
@@ -56,3 +56,8 @@ class Article(models.Model):
 class NewsLetterRecipients(models.Model):
     name = models.CharField(max_length = 30)
     email= models.EmailField()
+
+class MoringaMerch(models.Model):
+    name = models.CharField(max_length=40)
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=20)
